1947a1d204ec174c630841224c356692374fbbdb
Balasiva <DOPATHIBALASIVA@gmail.com>
2025-04-03 20:29:00 +0530

    Update adminPage.js

--- frontend/src/adminPage.js
+++ frontend/src/adminPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const base_url = `https://portfoliowebsite-kkap.onrender.com`;
+const base_url = `https://test-portfolio-qp55.onrender.com`;
 
 const AdminPage = () => {
   const [data, setData] = useState([]);
