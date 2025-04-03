f157ee3e4abc0ca81615ec17138d7efd354a7616
balasiva <dopathibalasiva@gmail.com.com>
2025-04-03 22:00:34 +0530

    render commit

--- frontend/src/mobileView/mobileApp.js
+++ frontend/src/mobileView/mobileApp.js
@@ -2,14 +2,14 @@ import React from "react";
 import MobileMain from "./mobileMain";
 import MobileHeader from "./mobileHeader";
 import { Routes, Route } from "react-router-dom";
+import AdminPage from "../adminPage.js";
 
 function MobileApp() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={[<MobileHeader />, <MobileMain />]} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={[<MobileHeader />, <MobileMain />]} />
+      <Route path="/adminPage" element={<AdminPage />} />
+    </Routes>
   );
 }
 
